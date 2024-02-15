2c61b35c253e83ba9066b1dc7a639435c7a1b45b
Anirban Banerjee <abanerjee763@outlook.com>
2024-02-15 10:54:20 +0530

    Some changes in routes

--- routes/journal.js
+++ routes/journal.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { runValidation } = require('../validators');
 const { journalcreatevalidation, ledgervalidation } = require('../validators/ledger');
 const { journalcreation } = require('../controllers/ledgerjournal');
+const { validateTokenMiddleware } = require('../controllers/validatetoken');
+const { companyMiddleware } = require('../controllers/auth');
 const router = express.Router();
 
-router.post('/create-journal-entry', journalcreatevalidation, runValidation, journalcreation);
+router.post('/create-journal-entry', journalcreatevalidation, runValidation,validateTokenMiddleware,companyMiddleware, journalcreation);
 
 
 
